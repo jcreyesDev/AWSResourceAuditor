efb9e7ecb72be95af3f833bddbc929f7a96c8c25
Julio Cesar Reyes <jcreyesdev@gmail.com>
2026-06-12 12:54:55 +0200

    fix: remove misplaced docstring in main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from auditor.account import get_account_info
 
 if __name__ == '__main__':
-    "Get AWS account information and print it to the console."
     account_info = get_account_info()
     for key, value in account_info.items():
         print(f"{key}: {value}")
